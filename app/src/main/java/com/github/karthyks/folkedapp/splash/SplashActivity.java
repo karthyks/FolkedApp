c1d89b879c4051447c1bbbc5b0aa1d7b0d25abbe
Karthik <karthy@Karthiks-MacBook-Pro.local>
2016-05-14 19:51:09 +0530

    initial commit

---
+++ app/src/main/java/com/github/karthyks/folkedapp/splash/SplashActivity.java
@@ -0,0 +1,9 @@
+package com.github.karthyks.folkedapp.splash;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by karthy on 5/14/16.
+ */
+public class SplashActivity extends AppCompatActivity {
+}
